39cefdc5ac711b7cef2ffb93913243e835368629
roshnins <roshnins@hotmail.com>
2022-09-23 15:39:29 +0800

    Changes to App.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import tkinter as tk
 import customtkinter as ctk
 
 from PIL import ImageTk
-#from authtoken import auth_token
+from authtoken import auth_token
 
 import torch
 from torch import autocast
@@ -22,6 +22,28 @@ prompt = ctk.CTkEntry(height=40, width=512, text_font=(
 # Position of the entry prompt
 prompt.place(x=10, y=10)
 
+# Placeholder to contain images
+lmain = ctk.CTkLabel(height=512, width=512)
+lmain.place(x=10, y=110)
 
+# Specify the model
+modelid = "CompVis/stable-diffusion-v1-4"
+device = "cuda"
+# Set the pipeline
+pipe = StableDiffusionPipeline.from_pretrained(
+    modelid, revision="fp16", torch_dtype=torch.float16, use_auth_token=auth_token)
+pipe.to(device)
+
+
+def generate():
+    pass
+
+
+# Create a button
+trigger = ctk.CTkButton(height=40, width=120, text_font=(
+    "Arial", 20), text_color="white", fg_color="blue", command=generate)
+# Text of button
+trigger.configure(text="Generate")
+trigger.place(x=206, y=60)
 # Run the app
 app.mainloop()
